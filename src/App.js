135950aa21374019529b8b77993f4e9cd4909cbc
Timur <Stafr95@mail.ru>
2024-09-03 17:26:12 +0500

    Creation App component

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+import Wrapper from "./components/Wrapper/Wrapper";
+
+const App = () => {
+		let [calc, setCalc] = useState({
+				sign: '',
+				num: 0,
+				res: 0,
+		});
+
+
+		return (
+				<Wrapper>
+
+				</Wrapper>
+		)
+
+}
+
+
+export default App;
