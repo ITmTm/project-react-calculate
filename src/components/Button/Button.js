8ccac192177d18edfebb936544d4d1c5c69f38f5
Timur <Stafr95@mail.ru>
2024-09-04 14:36:16 +0500

    Component Description

---
+++ src/components/Button/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './button.scss';
+
+const Button = ({ className, value, onClick }) => {
+		return (
+				<button className={className} onClick={onClick}>
+						{value}
+				</button>
+		);
+};
+
+export default Button;
