b5052502bfccb5dc8e32ee30298188ac56f0e1c5
Timur <Stafr95@mail.ru>
2024-09-04 14:36:12 +0500

    Component Description

---
+++ src/components/ButtonBox/ButtonBox.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './buttonBox.scss';
+
+const ButtonBox = ({ children }) => {
+		return <div className='buttonBox'>{children}</div>;
+};
+
+export default ButtonBox;
