9bc2a17daa0e42141a818ad8731e94c0f63c4cbf
Timur <Stafr95@mail.ru>
2024-09-05 16:21:10 +0500

    - Binary search for font size - Window resize handler (resize) - Using useCallback to optimize the adjustFontSize function

--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -1,25 +1,48 @@
-import React, { useRef, useEffect } from "react";
+import React, {useRef, useEffect, useCallback} from "react";
 
 import './screen.scss';
 
 const Screen = ({ value }) => {
 		const textRef = useRef(null);
 
-		useEffect(() => {
-				const adjustFontSize = () => {
-						const element = textRef.current;
-						let fontSize = 70;
+		const adjustFontSize = useCallback(() => {
+				const element = textRef.current;
+				if (!element) return;
 
-						element.style.fontSize = `${fontSize}px`;
+				let maxFontSize = 70;
+				let minFontSize = 1;
+				element.style.fontSize = `${maxFontSize}px`;
 
-						while (element.scrollWidth > element.clientWidth && fontSize > 1) {
-								fontSize--;
-								element.style.fontSize = `${fontSize}px`;
+				// Использование двоичного поиска для быстрого уменьшения размера шрифта
+				while (maxFontSize - minFontSize > 1) {
+						const midFontSize = Math.floor((minFontSize + maxFontSize) / 2);
+						element.style.fontSize = `${midFontSize}px`;
+
+						if (element.scrollWidth > element.clientWidth) {
+								maxFontSize = midFontSize;
+						} else {
+								minFontSize = midFontSize;
 						}
+				}
+
+				element.style.fontSize = `${minFontSize}px`;
+		}, [value]);
+
+		useEffect(() => {
+				adjustFontSize();
+		}, [value, adjustFontSize()]);
+
+		useEffect(() => {
+				const handleResize = () => {
+						adjustFontSize();
 				};
 
-				adjustFontSize();
-		}, [value]);
+				window.addEventListener('resize', handleResize);
+				return () => {
+						window.removeEventListener('resize', handleResize);
+				};
+		}, [adjustFontSize]);
+
 
 		return (
 				<div className='screen'>
